c1ffc48e7df5354a44e6bbad88a299d1357e5225
Nithyadevi <nithi2218@gmail.com>
2022-04-08 11:43:39 +1000

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: "Steve",
+    email: "steve@steve.com"
+  })
+})
+
+.then(res => res.json())
+.then(res => {
+
+
+    let newNode = document.createElement('div');
+    newNode.innerHTML = res.id;
+    document.querySelector("body").appendChild(newNode);
+
+
+})
+
+.catch(error => {
+  let newNode = document.createElement('div');
+  newNode.innerHTML = error.message;
+  document.querySelector("body").appendChild(newNode);
+
+})
+
+
+}
